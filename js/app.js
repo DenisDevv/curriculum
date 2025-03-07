03fa4fc2aa38b82f2ea49030302ee66c73109204
DenisDevv <karajedenis@gmail.com>
2025-03-07 09:30:51 +0100

    Creato sistema curriculum

---
+++ js/app.js
@@ -0,0 +1,44 @@
+function rimuovi(opzione) {
+    var elemento = document.getElementById(opzione);
+    elemento.parentNode.removeChild(elemento);
+}
+function aggiungi(opzione) {
+  switch (opzione) {
+    case "esperienze": {
+      let div = document.getElementById(opzione);
+      let newId = 'opzione' + (document.querySelectorAll('#' + opzione + ' > opzione').length + 1);
+      let newElement = document.createElement('opzione');
+      newElement.id = newId;
+      newElement.innerHTML = `
+        <label for="azienda">Azienda:</label>
+        <input type="text" id="azienda" name="azienda">
+        <label for="dal">Dal:</label>
+        <input type="text" id="dal" name="dal">
+        <label for="al">Al:</label>
+        <input type="text" id="al" name="al">
+        <button onclick="rimuovi('${newId}')">Rimuovi</button>
+      `;
+      div.appendChild(newElement);
+      break;
+    }
+    case "titoli": {
+      let div = document.getElementById(opzione);
+      let newId = 'opzione' + (document.querySelectorAll('#' + opzione + ' > opzione').length + 1);
+      let newElement = document.createElement('opzione');
+      newElement.id = newId;
+      newElement.innerHTML = `
+        <label for="titolo">Titolo:</label>
+        <input type="text" id="titolo" name="titolo">
+        <label for="scuola">Scuola:</label>
+        <input type="text" id="scuola" name="scuola">
+        <button onclick="rimuovi('${newId}')">Rimuovi</button>
+      `;
+      div.appendChild(newElement);
+      break;
+    }
+  }
+}
+function stampa() {
+  const element = document.body;
+  html2pdf().from(element).save('curriculum.pdf');
+}
